31f74d4a6fe3d846e4d7421fa702eb101f09fde2
Казус <kir.kondraschov@yandex.ru>
2016-04-17 17:01:33 +0300

    Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private double AnalyzeString(string str)
+        {
+            
+        }
+        private Tree SetTree(Tree leftTree, string str, Tree rightTree)
+        {
+            return new Tree(leftTree, if(GetChar(str) != null) ? str.Substring(GetChar(str),GetChar(str) : str), rightTree);
+        }
+        private int GetChar(string str)
+        {
+            int stat = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '(')
+                    stat++;
+                else if (str[i] == ')')
+                    stat--;
+                else if (str[i] == '+' || str[i] == '-' || str[i] == '/' || str[i] == '*')
+                    return i;
+            }
+            return null;
+        }
+        int marker1;
+        int marker2;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Calculator/Tree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Tree
+    {
+        string value;
+        Tree leftTree;
+        Tree rightTree;
+        public Tree(Tree leftTree, string value, Tree rightTree)
+        {
+            this.leftTree = leftTree;
+            this.value = value;
+            this.rightTree = rightTree;
+        }
+    }
+}
